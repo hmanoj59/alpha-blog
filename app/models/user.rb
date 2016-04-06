95b8c307ab133e263ad0c070bde4540502a2255e
hmanoj59 <hotasmail@gmail.com>
2016-04-05 17:45:06 -0700

    Association and debugger

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
-
-  validates :username, presence: true, uniqueness: {case_sensitive: false },
-  length: { minimum: 3, maximum:25 }
-   VALID_EMAIL_REGEX = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i
+  has_many :articles
+  before_save { self.email = email.downcase }
+  validates :username, presence: true,
+            uniqueness: {case_sensitive: false },
+            length: { minimum: 3, maximum:25 }
+  VALID_EMAIL_REGEX = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i
   validates :email, presence: true, length: {maximum: 100},
             uniqueness: {case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
