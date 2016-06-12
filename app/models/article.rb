9ac4714477654b468b855deeac705839739d9c54
hmanoj59 <hotasmail@gmail.com>
2016-06-12 00:53:16 -0700

    Added Left Nav in users_loggedin page, fixed errors in users, validations changed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-  validates :title, presence: true, length: {minimum: 3, maximum: 20}
-  validates :description, presence: true, length: {minimum: 5, maximum: 100}
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 5, maximum: 1000}
   validates :user_id, presence: true
 
 
